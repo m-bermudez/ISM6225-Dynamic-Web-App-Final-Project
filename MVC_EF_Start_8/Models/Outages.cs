8e70a8eaf31afc2b7a6f93d4c7ed1a8b4433a04b
Ryan <stopczynski@usf.edu>
2025-04-29 10:23:15 -0400

    Implemented API and Singleton Database into the project. Changed HomeController.cs, created and populated Outages.cs, created the Services folder, created NuclearOutageService.cs, populated NuclearOutageService.cs, updated program.cs, and updated startup.cs.

---
+++ MVC_EF_Start_8/Models/Outages.cs
@@ -0,0 +1,23 @@
+﻿namespace MVC_EF_Start_8.Models
+{
+    public class OutageRecord
+    {
+        public string period { get; set; }
+        public string facility { get; set; }
+        public string facilityName { get; set; }
+        public string generator { get; set; }
+        public string capacity { get; set; }
+        public string outage { get; set; }
+        public string percentOutage { get; set; }
+    }
+
+    public class Response
+    {
+        public List<OutageRecord> data { get; set; }
+    }
+
+    public class Root
+    {
+        public Response response { get; set; }
+    }
+}
---
+++ MVC_EF_Start_8/Services/NuclearOutageService.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MVC_EF_Start_8.Models;
+
+namespace MVC_EF_Start_8.Services
+{
+    public class NuclearOutageService
+    {
+        private readonly List<OutageRecord> _outageData = new List<OutageRecord>();
+        private bool _isDataFetched = false;
+
+        public bool IsDataFetched() => _isDataFetched;
+        public void MarkDataAsFetched() => _isDataFetched = true;
+
+        public async Task AddOutagesAsync(List<OutageRecord> data)
+        {
+            await Task.Run(() => _outageData.AddRange(data));
+        }
+
+        public async Task<List<OutageRecord>> GetAllOutagesAsync()
+        {
+            return await Task.FromResult(_outageData);
+        }
+
+        public async Task<List<OutageRecord>> GetOutageDataByPeriodAsync(string period)
+        {
+            return await Task.FromResult(_outageData.Where(d => d.period == period).ToList());
+        }
+    }
+}
